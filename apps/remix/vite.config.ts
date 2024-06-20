57283337e5d84b2132db6a3040729f20bbd11911
Andrea <andrea@yaaaw.it>
2024-06-20 12:03:33 +0200

    init

---
+++ apps/remix/vite.config.ts
@@ -0,0 +1,13 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { vercelPreset } from "@vercel/remix/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
+});
